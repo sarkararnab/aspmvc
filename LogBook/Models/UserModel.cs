e95b9f9a245243b2d6677e22a12592d38c686835
sarkararnab <sarkararnab@users.noreply.github.com>
2019-10-25 12:14:19 +0545

    first commit

---
+++ LogBook/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogBook.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public bool Gender { get; set; }
+        public string Contact { get; set; }
+    }
+}
---
+++ LogBook/Models/VehicleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogBook.Models
+{
+    public class Vehicle
+    {
+        public int VehicleID { get; set; }
+        public string Number { get; set; }
+        public DateTime IssuedOn { get; set; }
+        public User User { get; set; }
+        public int OwnedBy { get; set; }
+    }
+}
